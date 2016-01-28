04193f4849a4ae7319e3f9beacd3a3ac4de89a49
netbuffer <javawiki@163.com>
2016-01-28 15:47:33 +0800

    POI test

--- src/main/java/cn/com/ttblog/jfinal_bootstrap_table/util/POIExcelUtil.java
+++ src/main/java/cn/com/ttblog/jfinal_bootstrap_table/util/POIExcelUtil.java
@@ -2,55 +2,76 @@ package cn.com.ttblog.jfinal_bootstrap_table.util;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.util.WorkbookUtil;
+import org.apache.poi.ss.util.CellUtil;
 
 public class POIExcelUtil {
-	public static void test() {
+	public static void exec(String path) {
 		Workbook wb = new HSSFWorkbook(); // or new XSSFWorkbook();
 		CreationHelper createHelper = wb.getCreationHelper();
-		Sheet sheet1 = wb.createSheet("new sheet");
-		Row row = sheet1.createRow((short) 0);
-		// Create a cell and put a value in it.
+		Sheet sheet = wb.createSheet("用户信息");
+		// 冻结该行，使其无法移动
+		sheet.createFreezePane(0, 1, 0, 1);
+		// Header header = sheet1.getHeader();
+		// header.setCenter("Center Header");
+		// header.setLeft("Left Header");
+		// header.setRight(HSSFHeader.font("Stencil-Normal", "Italic") +
+		// HSSFHeader.fontSize((short) 16) +
+		// "Right w/ Stencil-Normal Italic font and size 16");
+		Row row = sheet.createRow((short) 0);
+		row.setHeightInPoints(30);
 		Cell cell = row.createCell(0);
-		cell.setCellValue(1);
+		CellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		cellStyle.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		cell.setCellValue("用户昵称");
+		cell.setCellStyle(cellStyle);
+		row.createCell(1).setCellValue("用户性别");
 
-		// Or do it on one line.
-		row.createCell(1).setCellValue(1.2);
+		CellStyle hlink_style = wb.createCellStyle();
+		Font hlink_font = wb.createFont();
+		hlink_font.setUnderline(Font.U_SINGLE);
+		hlink_font.setColor(IndexedColors.BLUE.getIndex());
+		hlink_style.setFont(hlink_font);
+		hlink_style.setAlignment(CellStyle.ALIGN_CENTER);
+		hlink_style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		Hyperlink link = createHelper.createHyperlink(Hyperlink.LINK_URL);
+		link.setAddress("http://poi.apache.org/");
+		cell.setHyperlink(link);
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.BORDER_TOP, CellStyle.BORDER_MEDIUM);
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.BORDER_BOTTOM, CellStyle.BORDER_MEDIUM);
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.BORDER_LEFT, CellStyle.BORDER_MEDIUM);
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.BORDER_RIGHT, CellStyle.BORDER_MEDIUM);
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.TOP_BORDER_COLOR,IndexedColors.RED.getIndex());
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.BOTTOM_BORDER_COLOR, IndexedColors.RED.getIndex());
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.LEFT_BORDER_COLOR,IndexedColors.RED.getIndex());
+		CellUtil.setCellStyleProperty(cell, wb, CellUtil.RIGHT_BORDER_COLOR, IndexedColors.RED.getIndex());
 		row.createCell(2).setCellValue(
-				createHelper.createRichTextString("This is a string"));
-		row.createCell(3).setCellValue(true);
+				createHelper.createRichTextString("This is a string用户年龄"));
+		row.createCell(3).setCellValue("用户手机");
+		row.createCell(4).setCellValue("收货地址");
+		row.createCell(5).setCellValue("注册时间");
+		sheet.autoSizeColumn(0); // adjust width of the first column
+		sheet.autoSizeColumn(1);
+		sheet.autoSizeColumn(2);
+		sheet.autoSizeColumn(3);
+		sheet.autoSizeColumn(4);
 
-		Sheet sheet2 = wb.createSheet("second sheet");
-		// Note that sheet name is Excel must not exceed 31 characters
-		// and must not contain any of the any of the following characters:
-		// 0x0000
-		// 0x0003
-		// colon (:)
-		// backslash (\)
-		// asterisk (*)
-		// question mark (?)
-		// forward slash (/)
-		// opening square bracket ([)
-		// closing square bracket (])
-
-		// You can use
-		// org.apache.poi.ss.util.WorkbookUtil#createSafeSheetName(String
-		// nameProposal)}
-		// for a safe way to create valid names, this utility replaces invalid
-		// characters with a space (' ')
-		String safeName = WorkbookUtil
-				.createSafeSheetName("[O'Brien's sales*?]"); // returns
-		Sheet sheet3 = wb.createSheet(safeName);
 		FileOutputStream fileOut;
 		try {
-			fileOut = new FileOutputStream("workbook.xls");
+			fileOut = new FileOutputStream(path);
 			wb.write(fileOut);
 			fileOut.close();
 		} catch (IOException e) {
--- src/test/java/cn/com/ttblog/jfinal_bootstrap_table/util/TestPOI.java
+++ src/test/java/cn/com/ttblog/jfinal_bootstrap_table/util/TestPOI.java
@@ -1,14 +1,44 @@
 package cn.com.ttblog.jfinal_bootstrap_table.util;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import org.junit.Test;
 
 public class TestPOI {
 
 	@Test
-	public void tt() {
-		POIExcelUtil.test();
+	public void tt() throws IOException {
+		File directory = new File("");// 参数为空
+		String projectpath=directory.getAbsolutePath();
+		POIExcelUtil.exec(projectpath+File.separator+"aa.xls");
 	}
 
+	public void sysoutpath() throws IOException {
+		// 第一种：获取类加载的根路径
+		File f = new File(this.getClass().getResource("/").getPath());
+		System.out.println(f);
+		// 获取当前类的所在工程路径; 如果不加“/” 获取当前类的加载目录
+		File f2 = new File(this.getClass().getResource("").getPath());
+		System.out.println(f2);
+
+		// 第二种获取项目路径
+		File directory = new File("");// 参数为空
+		System.out.println(directory.getAbsolutePath());
+		String courseFile = directory.getCanonicalPath();
+		System.out.println(courseFile);
+
+		// 第三种
+		URL xmlpath = this.getClass().getClassLoader().getResource("");
+		System.out.println(xmlpath);
+
+		// 第四种
+		System.out.println(System.getProperty("user.dir"));
+		/*
+		 * 获取当前工程路径
+		 */
+		// 第五种： 获取所有的类路径 包括jar包的路径
+		System.out.println(System.getProperty("java.class.path"));
+	}
 }
