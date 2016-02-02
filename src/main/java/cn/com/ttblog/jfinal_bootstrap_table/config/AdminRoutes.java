128af815c5d85717cb871f1d619c1da94fd30c1c
netbuffer <javawiki@163.com>
2016-02-02 21:19:33 +0800

    ehcache demo

--- src/main/java/cn/com/ttblog/jfinal_bootstrap_table/config/AdminRoutes.java
+++ src/main/java/cn/com/ttblog/jfinal_bootstrap_table/config/AdminRoutes.java
@@ -1,6 +1,8 @@
 package cn.com.ttblog.jfinal_bootstrap_table.config;
 
+import cn.com.ttblog.jfinal_bootstrap_table.controller.CacheController;
 import cn.com.ttblog.jfinal_bootstrap_table.controller.UserController;
+
 import com.jfinal.config.Routes;
 
 /**
@@ -12,6 +14,7 @@ public class AdminRoutes extends Routes {
 	@Override
 	public void config() {
 		add("/user",UserController.class,"/");
+		add("/cache",CacheController.class,"/");
 	}
 
 }
--- src/main/java/cn/com/ttblog/jfinal_bootstrap_table/controller/UserController.java
+++ src/main/java/cn/com/ttblog/jfinal_bootstrap_table/controller/UserController.java
@@ -1,15 +1,19 @@
 package cn.com.ttblog.jfinal_bootstrap_table.controller;
 
 import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import cn.com.ttblog.jfinal_bootstrap_table.interceptor.TimeInterceptor;
 import cn.com.ttblog.jfinal_bootstrap_table.model.User;
 import cn.com.ttblog.jfinal_bootstrap_table.service.IUserService;
 import cn.com.ttblog.jfinal_bootstrap_table.service.impl.UserServiceImpl;
+
 import com.jfinal.aop.Before;
 import com.jfinal.aop.Enhancer;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.ehcache.CacheInterceptor;
 
 /**
  * user控制器
@@ -26,6 +30,10 @@ public class UserController extends Controller {
 		render("index.html");
 	}
 	
+	/**
+	 * 开启缓存
+	 */
+	@Before({CacheInterceptor.class})
 	public void userlist() {
 		int limit=getParaToInt("limit");
 		int offset=getParaToInt("offset");
